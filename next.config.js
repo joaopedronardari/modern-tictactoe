2f323bac015301ae85dda214a882326a66c7e23f
João Pedro Nardari dos Santos <joaonardari@gmail.com>
2024-11-23 16:21:33 -0300

    Remove string-replace-loader configuration

--- next.config.js
+++ next.config.js
@@ -4,23 +4,6 @@ const nextConfig = {
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
-  },
-  // Add custom webpack config to ignore certain data attributes
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: [
-        {
-          loader: 'string-replace-loader',
-          options: {
-            search: 'data-gr-ext-installed|data-new-gr-c-s-check-loaded',
-            replace: '',
-            flags: 'g'
-          }
-        }
-      ]
-    });
-    return config;
   }
 }
 
